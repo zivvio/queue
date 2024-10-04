12ae38097eea9df51d03fb06225d7e05ccb98695
charescape <53265646+charescape@users.noreply.github.com>
2024-10-04 17:40:07 +0800

    tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,7 @@ $_SERVER['SCRIPT_NAME'] = '/' . __DIR__;
 $_SERVER['SCRIPT_FILENAME'] = __FILE__;
 
 require_once(__DIR__ . '/../vendor/autoload.php');
-require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+require_once(__DIR__ . '/../vendor/zivvio/zivvio/framework/Yii.php');
 
 Yii::setAlias('@yii/queue', dirname(__DIR__) . '/src');
 Yii::setAlias('@yii/queue/amqp', dirname(__DIR__) . '/src/drivers/amqp');
